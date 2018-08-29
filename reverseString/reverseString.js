1049a9fe1ce6bd271bb0718dd9973851a5be23c4
Zara <zotamias@gmail.com>
2018-08-29 03:46:54 -0400

    JavaScript algorithm problems

---
+++ reverseString/reverseString.js
@@ -0,0 +1,19 @@
+// Reverse A String
+
+/*
+Used built-in functions in order to solve this algorithm.
+-split() method splits the string into an array of characters
+-reverse() method reverses the array
+-join() method joins all of the arrays into a string
+*/
+
+
+function reverseString(str) {
+  return str.split([]).reverse().join([]);
+}
+
+reverseString("hello");
+
+//reverseString("hello"); becomes "olleh"
+//reverseString("Howdy"); becomes "ydwoH"
+//reverseString("Greetings from Earth"); becomes "htraE morf sgniteerG"
